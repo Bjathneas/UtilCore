3d487565605c18d4d33a01b46e59cb506a524e99
Bjathneas <bjathneas@gmail.com>
2023-05-22 02:02:47 -0500

    oops text editor messed up

--- include/UtilCore/strings.h
+++ include/UtilCore/strings.h
@@ -3,7 +3,7 @@
 #include <string>
 
 namespace UtilCore {
-constinit char *kValidAlphabetChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+constexpr std::string kValidAlphabetChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 void purge_special(std::string &str);
 void tolower(std::string &str);
