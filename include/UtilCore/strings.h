a22083198a95a778e49e37430ddf653b483c3cdc
Bjathneas <bjathneas@gmail.com>
2023-05-22 02:04:02 -0500

    fixing const

--- include/UtilCore/strings.h
+++ include/UtilCore/strings.h
@@ -3,7 +3,7 @@
 #include <string>
 
 namespace UtilCore {
-constexpr std::string kValidAlphabetChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+std::string kValidAlphabetChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 void purge_special(std::string &str);
 void tolower(std::string &str);
